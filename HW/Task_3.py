a1cc8574f8ebb4c42d7641ff05867b762ea2ce16
Galina Stepanova <galina_golubczova@mail.ru>
2022-07-11 01:16:32 +0300

    First homework

---
+++ HW/Task_3.py
@@ -0,0 +1,35 @@
+# Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер 
+# четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+
+# Пример:
+
+# - x=34; y=-30 -> 4
+# - x=2; y=4-> 1
+# - x=-34; y=-30 -> 3
+
+#import pretty_errors
+
+x=int(input('Enter the value of X: '))
+y=int(input('Enter the value of Y: '))
+
+def prov(a):
+    while(a==0):
+        print('Error!')
+        a=int(input('Enter the value of X: '))
+        return a
+
+prov(x)
+prov(y)
+
+if (x>0 and y>0):
+    print('I quarter')
+elif( x<0 and y>0):
+    print('II quarter')
+elif(x<0 and y<0):
+    print('III quarter')
+elif (x>0 and y<0):
+    print('IV quarter')
+
+
+
+
---
+++ HW/Task_4.py
@@ -0,0 +1,24 @@
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+#from termios import B50
+
+a=int(input('Enter the number of the quarter: '))
+
+if (a==1):
+    print('x>0; y>0')
+elif(a==2):
+    print('x<0; y>0')
+elif(a==3):
+    print('x<0 and y<0')
+elif (a==4):
+    print('x>0 and y<0')
+
+
+
+
+    
+
+ 
+
+
+
+
